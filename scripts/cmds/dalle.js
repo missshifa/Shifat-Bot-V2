cef6e111e0ca42345566af2c7fb0855d1b0bf150
SHIFAT <nilaafrin733@gmail.com>
2025-09-27 07:18:07 +0600

    Update dalle.js

--- scripts/cmds/dalle.js
+++ scripts/cmds/dalle.js
@@ -5,7 +5,7 @@ const baseApiUrl = async () => {
 }; 
 module.exports = {
   config: {
-    name: "dalle",
+    name: "imgen",
     aliases: ["bing", "create", "imagine"],
     version: "1.0",
     author: "Dipto",
@@ -17,7 +17,7 @@ module.exports = {
   }, 
   onStart: async({ api, event, args }) => {
     const prompt = (event.messageReply?.body.split("dalle")[1] || args.join(" ")).trim();
-    if (!prompt) return api.sendMessage("❌| Wrong Format. ✅ | Use: 17/18 years old boy/girl watching football match on TV with 'Dipto' and '69' written on the back of their dress, 4k", event.threadID, event.messageID);
+    if (!prompt) return api.sendMessage("❌| Wrong Format. ✅ | Use: 17/18 years old boy/girl watching football match on TV with 'Shifat' and '69' written on the back of their dress, 4k", event.threadID, event.messageID);
     try {
        //const cookies = "cookies here (_U value)";
 const cookies = ["1WMSMa5rJ9Jikxsu_KvCxWmb0m4AwilqsJhlkC1whxRDp2StLDR-oJBnLWpoppENES3sBh9_OeFE6BT-Kzzk_46_g_z_NPr7Du63M92maZmXZYR91ymjlxE6askzY9hMCdtX-9LK09sUsoqokbOwi3ldOlm0blR_0VLM3OjdHWcczWjvJ78LSUT7MWrdfdplScZbtHfNyOFlDIGkOKHI7Bg"];
@@ -28,7 +28,7 @@ const imageUrls = response.data.imgUrls || [];
       if (!imageUrls.length) return api.sendMessage("Empty response or no images generated.", event.threadID, event.messageID);
       const images = await Promise.all(imageUrls.map(url => axios.get(url, { responseType: 'stream' }).then(res => res.data)));
     api.unsendMessage(wait.messageID);
-   api.sendMessage({ body: `✅ | Here's Your Generated Photo 😘`, attachment: images }, event.threadID, event.messageID);
+   api.sendMessage({ body: `| Here's Your Generated Photo 😘`, attachment: images }, event.threadID, event.messageID);
     } catch (error) {
       console.error(error);
       api.sendMessage(`Generation failed!\nError: ${error.message}`, event.threadID, event.messageID);
